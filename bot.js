3687f2ef814e8dcacb3f7cf3927c1b1d40f745a3
Blake Wyatt <nabakin@protonmail.ch>
2019-09-19 20:08:32 -0400

    Add support for reaction roles

--- bot.js
+++ bot.js
@@ -2,10 +2,13 @@ const Discord = require('discord.js')
 const { token } = require('./token.json')
 const client = new Discord.Client()
 const commandHandler = require('./commands/commandHandler')
+const roleReactionHandler = require('./roles/roleReactionHandler')
 
 client.on('ready', () => {
-  console.log('Bot is ready!');
+  console.log('Bot is ready!')
 })
+
+client.on('raw', async event => roleReactionHandler(event, client))
 
 client.on('message', async message => commandHandler(message))
 
