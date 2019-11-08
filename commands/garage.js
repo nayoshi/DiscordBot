04d72b4cc534e185e8ced149f6a1ab24d0c271c5
Matt Bautista <nayoshi12@gmail.com>
2019-11-08 15:26:17 -0500

    Implemented the embed system for the parking system. Added percentage to garage.json

--- commands/garage.js
+++ commands/garage.js
@@ -1,40 +1,28 @@
-// Credit to Space and hacker-alliance for the code
 
-// Written by Abrahan Nevarez for KnightHacks@UCF
-// Displays parking data with percentages full
-
-// Installs request necessary for HTTP calls
 const request = require('request')
 
-// Installs cherio, needed for JSON data
+const Discord = require('discord.js')
 const cheerio = require('cheerio')
 
 const URL = 'https://secure.parking.ucf.edu/GarageCount/iframe.aspx'
 
 const fs = require('fs')
 
-// Number of Garage Spots Available
-
 const garageCache = require('../constants/garage')
 const { garageTime } = require('../constants/config')
 module.exports = async function (command) {
   const garageData = await requestGarageData(5)
-  // console.log(garageData)
-  // console.log(garageTime)
-  // Gets the data of available garages from web page
-  // Beginning of a message
-  // TODO Embedded Message rather than simple message
-  let message = '```'
-  // For each key in the JS Object, construct the message while doing math on the fly.
+  const embed = new Discord.RichEmbed()
+    .setColor('#55555')
+    .setTitle('UCF Garage Status')
+
   Object.keys(garageData).forEach((value, index) => {
-    // The math is (max - available) / max * 100
-    message += 'Garage ' + value + ': ' + garageData[value].space + ' out of ' + garageData[value].total +
-          ' spaces available ' + (Math.round((((garageData[value].total - garageData[value].space) / garageData[value].total) * 100))) + '% full '
-    message += '\n'
+    embed.addField('Garage ' + value, garageData[value].space + '/' + garageData[value].total + ' (' + garageData[value].percentage + '%)', true)
   })
-  message += '```'
+  const cachedDate = new Date(garageCache.nextCache - garageTime)
+  embed.setFooter('Cached at ' + cachedDate.toLocaleDateString('en-US', { dateStyle: 'full', timeZone: 'America/New_York' }) + ' ' + cachedDate.toLocaleTimeString('en-US', { dateStyle: 'full', timeZone: 'America/New_York' }))
   // Sends the message so that user will be notified
-  command.message.channel.send(message)
+  command.message.channel.send(embed)
 }
 
 function requestGarageData () {
@@ -42,36 +30,29 @@ function requestGarageData () {
   return new Promise((resolve, reject) => {
     const currentTime = (new Date()).getTime()
     const garageFile = 'constants/garage.json'
-    console.log(garageCache.lastCached > currentTime + garageTime)
-    if (garageCache.lastCached > currentTime + garageTime) {
+    if (garageCache.nextCache < currentTime) {
       request(URL, (error, response, body) => {
         const $ = cheerio.load(body)
         if (error) {
           console.error(error)
           return
         }
-        // TODO Get the number of garages from website
-        // Loops 7 times because that's how many garages exists
         for (let i = 0; i < 7; i++) {
-        // Gets every single row by their id
           const row = $('#gvCounts_DXDataRow' + i).text()
-          // Regex match of `Garage *` and capture the wildcard
           const garage = row.match(/(Garage)\s*(\w*)/)[2]
-          // Initialize the json object
           garageAvail[garage] = {}
-          // Regex match of `num1/num2` and num 1 is the free space and num2 is the total number of spots
           const num = row.match(/(\d*)\/(\d*)/)
           garageAvail[garage].space = parseInt(num[1])
           garageAvail[garage].total = parseInt(num[2])
-          // Corrects for the misnumbering of the parking spaces. As the number of parking garages does not match with the actual number.
           if (garageAvail[garage].space >= garageAvail[garage].total) {
             garageAvail[garage].space = garageAvail[garage].total
           } else if (garageAvail[garage].space <= 0) {
             garageAvail[garage].space = 0
           }
+          garageAvail[garage].percentage = Math.round((((garageAvail[garage].total - garageAvail[garage].space) / garageAvail[garage].total) * 100))
         }
         garageCache.garages = garageAvail
-        garageCache.lastCached = currentTime
+        garageCache.nextCache = currentTime + garageTime
         fs.writeFile(garageFile, JSON.stringify(garageCache, null, 2), (err) => {
           if (err) {
             reject(err)
